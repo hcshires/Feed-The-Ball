64644c825b42a7bfacf117720377b49fd4871852
Henry Shires <hcsgeek@gmail.com>
2019-04-21 22:38:11 -0500

    AP Create Updates
    Updates for Meeting Abstraction requirement.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -8,7 +10,7 @@ public class GameController : MonoBehaviour
     public float friction;
     public float speed;
     protected int score;
-    protected int levelChange;
+    protected int debug;
 
     /* GameObjects */
     private Rigidbody rb;
@@ -19,33 +21,34 @@ public class GameController : MonoBehaviour
 
     /* Local Values */
     private Vector3 playerPosition;
-    private bool isInstRunning = false;
+    private bool isInstRunning = false; // Boolean for if instructions are on screen
     private string level;
+    private bool checkpoint = false;
 
-    void Awake() // Prevents GameObjects from resetting upon Level Change
+    void Awake() // Prevents GameObjects from resetting upon Level update
     {
         DontDestroyOnLoad(gameObject);
         score = 0;
-        levelChange = 1;
+        debug = 2;
     }
 
     void Start() // Initialization
     {
         rb = GetComponent<Rigidbody>();
+
     }
 
     void Update() // Update information displayed
     {
-        SetText(levelChange);
         MenuSelect();
+        SetText();
     }
 
-    void LevelChange() // Handles Level Transitions
+    void LevelChange(int lvl = 1) // Handles Level Transitions
     {
-        levelChange++;
-        level = "Level " + levelChange.ToString();
+        level = "Level " + lvl.ToString(); // Scene name
         SceneManager.LoadScene(level);
-        rb.position = Vector3.zero;
+        rb.position = Vector3.zero; // Reset position
         isInstRunning = false;
     }
 
@@ -82,75 +85,69 @@ public class GameController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             score++;
+            SetText(true, false);
         }
         if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("EnemyZ")) // Hits enemies
         {
             rb.position = Vector3.zero;
         }
+        if (other.gameObject.CompareTag("Checkpoint")) // Reaches checkpoint
+        {
+
+            SetText(true, true); // Handle level and text when a checkpoint is reached
+        }
     }
 
-    void SetText(int level = 1) // Sets text to the value of score
+    void SetText(bool update = false, bool checkpoint = false) // Sets text to the value of score
     {
         scoreText.text = "Score: " + score.ToString();
 
-        switch (score) // When to change levels
+        if (score < 12) // Prevents initial text from remaining on screen when function is called by update()
         {
-            case 12:
-                if (levelChange == 1)
-                {
-                    LevelChange();
-                }
-                break;
-            case 34:
-                if (levelChange == 2)
-                {
-                    LevelChange();
-                }
-                break;
-            case 58:
-                if (levelChange == 3)
-                {
-                    LevelChange();
-                }
-                break;
+            directionText.text = "Welcome to Feed the Ball! To move the ball, press W, A, S, D or Arrow Keys.";
         }
-        switch (level)
+
+        // When to update levels
+        if (score == 12 && update == true) // 1
         {
-            case 1:
-                directionText.text = "Welcome to Feed the Ball! To move the ball, press W, A, S, D or Arrow Keys.";
-                break;
-            case 2:
-                if (isInstRunning == false)
-                {
-                    isInstRunning = true;
-                    directionText.text = "Avoid red enemies and collect all coins to advance to the next level. Good Luck!";
-                }
-
-                if (playerPosition.z >= 10 && isInstRunning == true)
-                {
-                    directionText.text = "";
-                }
-                break;
-            case 3:
-                if (isInstRunning == false)
-                {
-                    isInstRunning = true;
-                    directionText.text = "Watch out for red platforms that act like enemies!";
-                }
-
-                if (playerPosition.z >= 10 && isInstRunning == true)
-                {
-                    directionText.text = "";
-                }
-                break;
+            LevelChange(2);
+            isInstRunning = true;
+            directionText.text = "Avoid red enemies and collect all coins to advance to the next level. Good Luck!";
         }
+
+        else if (score == 34 && checkpoint == true) // 2
+        {
+            LevelChange(3);
+            isInstRunning = true;
+            directionText.text = "Watch out for red platforms that act like enemies!";
+
+        }
+        else if (score == 59 && checkpoint == true) // 3
+        {
+            /* LevelChange(4); Currently not added */
+            rb.position = Vector3.zero;
+            directionText.text = "YOU WIN!";
+        }
+
+        else if (playerPosition.z >= 10 && isInstRunning == true)
+        {
+            directionText.text = "";
+        }
+
     }
 
     void MenuSelect()
     {
         if (Input.GetKeyUp("l")) // Quick Skip to levels for debugging
         {
-            LevelChange();
+            LevelChange(debug);
+            score += 12;
+            debug++;
+        }
+
+        if (Input.GetKeyUp("q"))
+        {
+            score += 22;
         }
 
         if (Input.GetKeyUp("m")) // Level Menu
